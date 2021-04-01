eec4bcc8fe3ea4b02b2051cd9cd247e6b37b307f
pguyal <pguyal@hotmail.com>
2021-04-01 15:57:19 -0400

    Create sign-out event handler in app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -10,4 +10,5 @@ console.log('authEvents', authEvents)
 $(() => {
   $('#sign-up').on('submit', authEvents.onSignUp)
   $('#sign-in').on('submit', authEvents.onSignIn)
+  $('#sign-out').on('click', authEvents.onSignOut)
 })
