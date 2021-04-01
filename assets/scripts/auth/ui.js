a69fa2ff9617a52578c973dbc758933517e66228
pguyal <pguyal@hotmail.com>
2021-04-01 15:32:19 -0400

    Create ui messages for sign in success and establish require path to store.js

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,7 +1,15 @@
 'use strict'
 
+const store = require('../store')
+
 const onSignUpSuccess = function () {
   $('#message').text('Account successfully created!')
+  $('form').trigger('reset')
+}
+
+const onSignInSuccess = function (response) {
+  store.user = response.user
+  $('#message').text(response.user.email + ' signed in successfully')
   $('form').trigger('reset')
 }
 
@@ -12,5 +20,6 @@ const onError = function (err) {
 
 module.exports = {
   onSignUpSuccess,
+  onSignInSuccess,
   onError
 }
