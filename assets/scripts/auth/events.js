c73e682d933a79ea878c4a8509452e49ea5c7702
pguyal <pguyal@hotmail.com>
2021-04-01 12:19:04 -0400

    Create onSignUp event and establish require paths
    Made onSignUp event function and had it exported to app.js. Also
    created require paths from events.js to api.js and ui.js

---
+++ assets/scripts/auth/events.js
@@ -0,0 +1,21 @@
+'use strict'
+const api = require('./api')
+const ui = require('./ui')
+const getFormFields = require('../../../lib/get-form-fields')
+
+const onSignUp = function (event) {
+  // prevents the page from refreshing
+  event.preventDefault()
+  // selecting the form that was submitted
+  const form = event.target
+  // creating a js object from the forms input values
+  const formData = getFormFields(form)
+  // pass form data to the api call
+  api.signUp(formData)
+    .then(ui.onSignUpSuccess)
+    .catch(ui.onError)
+}
+
+module.exports = {
+  onSignUp
+}
