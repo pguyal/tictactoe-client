8168ee2e9281bdae0ca5a408bfa8a718da861bd5
pguyal <pguyal@hotmail.com>
2021-04-01 11:38:11 -0400

    Change apiUrl in config.js and sign-up.sh
    Due to production url being changed, had to make changes and
    retest sign up curl script. Note: curl script was successful.

--- curl-scripts/auth/sign-up.sh
+++ curl-scripts/auth/sign-up.sh
@@ -1,4 +1,4 @@
-curl "https://library-express-api.herokuapp.com/sign-up" \
+curl "https://tic-tac-toe-api-production.herokuapp.com/sign-up" \
   --include \
   --request POST \
   --header "Content-Type: application/json" \
