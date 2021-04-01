46387e60351a68b810049c6b78afc6c93154620b
pguyal <pguyal@hotmail.com>
2021-04-01 12:25:02 -0400

    Create api sign up function and establish require path to config.js

---
+++ assets/scripts/auth/api.js
@@ -0,0 +1,13 @@
+const config = require('../config')
+
+const signUp = function (formData) {
+  return $.ajax({
+    method: 'POST',
+    url: config.apiUrl + '/sign-up',
+    data: formData
+  })
+}
+
+module.exports = {
+  signUp
+}
