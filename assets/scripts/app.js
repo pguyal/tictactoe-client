7845a96cf38f4232d05bcd3d0764176511bd6f06
pguyal <pguyal@hotmail.com>
2021-04-02 14:54:52 -0400

    Create click event handler for play new game button and establish require file path to game/events.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,6 +3,7 @@
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 const authEvents = require('./auth/events')
+const gameEvents = require('./game/events')
 console.log('authEvents', authEvents)
 // use require without a reference to ensure a file is bundled
 // require('./example')
@@ -14,4 +15,5 @@ $(() => {
   $('#sign-up').on('submit', authEvents.onSignUp)
   $('#sign-in').on('submit', authEvents.onSignIn)
   $('#sign-out').on('click', authEvents.onSignOut)
+  $('#play-game').on('click', gameEvents.onPlayGame)
 })
