99267005ef8f0da07d4ee76f33cd36caff9564e3
pguyal <pguyal@hotmail.com>
2021-04-06 14:39:17 -0400

    Update onBoardClick function
    Added the ability to insert X's and O's to game board. Created the
    ability to change players along with corresponding player turn messages.

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -9,6 +9,38 @@ const onPlayGame = function (event) {
     .catch(ui.onError)
 }
 
+let currentPlayer = 'X'
+// const index = event.target.id
+const onBoardClick = function (event) {
+  const space = $(event.target)
+  if (space.text() === '') {
+    space.text(currentPlayer)
+    currentPlayer = currentPlayer === 'O' ? 'X' : 'O'
+    // if (currentPlayer === 'X') {
+    // currentPlayer = 'O'
+    // } else {
+    // currentPlayer = 'X'
+    // }
+  } else {
+    $('#message').text('That space is taken')
+    $('#message').addClass('playerTurn')
+    setTimeout(() => {
+      $('#message').text('')
+      $('#message').removeClass('playerTurn')
+    }, 2000)
+  }
+  if (currentPlayer === 'X') {
+    $('#game-message').text('Player One\'s Turn')
+  } else if (currentPlayer === 'O') {
+    $('#game-message').text('Player Two\'s Turn')
+  }
+}
+// api.boardClick()
+//   .then(ui.onBoardClickSuccess)
+//   .catch(ui.onError)
+// }
+
 module.exports = {
-  onPlayGame
+  onPlayGame,
+  onBoardClick
 }
